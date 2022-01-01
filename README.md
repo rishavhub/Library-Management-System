27f601eb219d72d3e7767f7bafe45e81512bd87f
Rishav Banerjee <83203285+rishavhub@users.noreply.github.com>
2022-01-02 01:32:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Library-Management-System
-This project is built on HTML, CSS, PHP, SQL
+I made this project with my friend in 5th sem, while doing my BSc Honours. I developed the frontend and he developed the backend. 
---
+++ database/lms.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 31, 2021 at 08:01 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE `author` (
+  `BookId` int(10) NOT NULL,
+  `Author` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`BookId`, `Author`) VALUES
+(3, 'Alok'),
+(4, 'Kirmada'),
+(5, 'xyz'),
+(6, 'Bheem'),
+(7, 'Doraemon'),
+(7, 'Raju'),
+(8, 'Balaram'),
+(8, 'krishna'),
+(9, 'Radha');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `BookId` int(10) NOT NULL,
+  `Title` varchar(50) DEFAULT NULL,
+  `Publisher` varchar(50) DEFAULT NULL,
+  `Year` varchar(50) DEFAULT NULL,
+  `Availability` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`BookId`, `Title`, `Publisher`, `Year`, `Availability`) VALUES
+(1, 'Operating System', 'Pearson', '2006', 2),
+(2, 'DBMS', 'Pearson', '2010', 0),
+(7, 'Discrete Structures', 'Pearson', '2010', 10),
+(8, 'Database Processing', 'Prentice Hall', '2013', 12),
+(9, 'Computer System Architecture', 'Prentice Hall', '2015', 4),
+(10, 'C: How to program', 'Prentice Hall', '2009', 3),
+(11, 'Atomic and Nuclear Systems', 'Pearson', '2017', 13),
+(13, 'General Theory of Relativity', 'Pearson', '2012', 5),
+(14, 'Heat and Thermodynamics', 'Pearson', '2013', 9),
+(15, 'Machine Design', 'Pearson', '2012', 5),
+(16, 'Nuclear Physics', 'Pearson', '1998', 7),
+(17, 'Operating System', 'Pearson', '1990', 7),
+(18, 'Theory of Relativity', 'Pearson', '1992', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `M_Id` int(10) NOT NULL,
+  `RollNo` varchar(50) DEFAULT NULL,
+  `Msg` varchar(255) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `Time` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recommendations`
+--
+
+CREATE TABLE `recommendations` (
+  `R_ID` int(10) NOT NULL,
+  `Book_Name` varchar(50) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `RollNo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `record`
+--
+
+CREATE TABLE `record` (
+  `RollNo` varchar(50) NOT NULL,
+  `BookId` int(10) NOT NULL,
+  `Date_of_Issue` date DEFAULT NULL,
+  `Due_Date` date DEFAULT NULL,
+  `Date_of_Return` date DEFAULT NULL,
+  `Dues` int(10) DEFAULT NULL,
+  `Renewals_left` int(10) DEFAULT NULL,
+  `Time` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `renew`
+--
+
+CREATE TABLE `renew` (
+  `RollNo` varchar(50) NOT NULL,
+  `BookId` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return`
+--
+
+CREATE TABLE `return` (
+  `RollNo` varchar(50) NOT NULL,
+  `BookId` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `RollNo` varchar(50) NOT NULL,
+  `Name` varchar(50) DEFAULT NULL,
+  `Type` varchar(50) DEFAULT NULL,
+  `Category` varchar(50) DEFAULT NULL,
+  `EmailId` varchar(50) DEFAULT NULL,
+  `MobNo` bigint(11) DEFAULT NULL,
+  `Password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`RollNo`, `Name`, `Type`, `Category`, `EmailId`, `MobNo`, `Password`) VALUES
+('1231', 'Soumik Kundu', 'Student', 'GEN', 'soumikkundu@gmail.com', 7278675325, '123'),
+('497', 'Ramen Manna', 'Student', 'GEN', 'ramenmanna@gmail.com', 7278514730, '123'),
+('636', 'Jagriti Pramanik', 'Student', 'GEN', 'jagritipramanik@gmail.com', 7059084962, '123'),
+('679', 'Sonali Prasad', 'Student', 'SC', 'sonaliprasad@gmail.com', 8100248269, '123'),
+('admin', 'Admin-Rishav', 'Admin', NULL, 'rishavbanerjeeofficial@gmail.com', 8777432495, '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`BookId`,`Author`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`BookId`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`M_Id`),
+  ADD KEY `RollNo` (`RollNo`);
+
+--
+-- Indexes for table `recommendations`
+--
+ALTER TABLE `recommendations`
+  ADD PRIMARY KEY (`R_ID`),
+  ADD KEY `RollNo` (`RollNo`);
+
+--
+-- Indexes for table `record`
+--
+ALTER TABLE `record`
+  ADD PRIMARY KEY (`RollNo`,`BookId`),
+  ADD KEY `BookId` (`BookId`);
+
+--
+-- Indexes for table `renew`
+--
+ALTER TABLE `renew`
+  ADD PRIMARY KEY (`RollNo`,`BookId`),
+  ADD KEY `BookId` (`BookId`);
+
+--
+-- Indexes for table `return`
+--
+ALTER TABLE `return`
+  ADD PRIMARY KEY (`RollNo`,`BookId`),
+  ADD KEY `BookId` (`BookId`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`RollNo`),
+  ADD UNIQUE KEY `EmailId` (`EmailId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `BookId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `M_Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `recommendations`
+--
+ALTER TABLE `recommendations`
+  MODIFY `R_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
